9cef7f40ec47343306c26d970b55da8a3262f0e5
Bruno Martins <bgcngm@gmail.com>
2018-09-17 13:53:31 +0100

    MoKeeSettingsActivity: Remove useless check for navbar toggle
     * The only condition for "Use on screen navigation keys instead
       of hardware keys" option to be shown is the availability
       of KeyDisabler feature. There is really no need to check if the
       device "needs" a software navbar (because it has no hardware keys).
    
    Change-Id: Id716d766927f440292a6d18d8fa77bed76ad90b3

--- src/org/mokee/setupwizard/MoKeeSettingsActivity.java
+++ src/org/mokee/setupwizard/MoKeeSettingsActivity.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2016 The CyanogenMod Project
- * Copyright (C) 2017 The LineageOS Project
- * Copyright (C) 2017 The MoKee Open Source Project
+ * Copyright (C) 2017-2018 The LineageOS Project
+ * Copyright (C) 2017-2018 The MoKee Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -58,7 +58,7 @@ public class MoKeeSettingsActivity extends BaseSetupWizardActivity {
     private CheckBox mNavKeys;
     private CheckBox mPrivacyGuard;
 
-    private boolean mHideNavKeysRow = false;
+    private boolean mSupportsKeyDisabler = false;
 
     private View.OnClickListener mNavKeysClickListener = view -> {
         boolean checked = !mNavKeys.isChecked();
@@ -104,18 +104,12 @@ public class MoKeeSettingsActivity extends BaseSetupWizardActivity {
         View navKeysRow = findViewById(R.id.nav_keys);
         navKeysRow.setOnClickListener(mNavKeysClickListener);
         mNavKeys = (CheckBox) findViewById(R.id.nav_keys_checkbox);
-        boolean needsNavBar = true;
-        try {
-            IWindowManager windowManager = WindowManagerGlobal.getWindowManagerService();
-            needsNavBar = windowManager.needsNavigationBar();
-        } catch (RemoteException e) {
-        }
-        mHideNavKeysRow = hideKeyDisabler(this);
-        if (mHideNavKeysRow || needsNavBar) {
-            navKeysRow.setVisibility(View.GONE);
-        } else {
+        mSupportsKeyDisabler = isKeyDisablerSupported(this);
+        if (mSupportsKeyDisabler) {
             boolean navKeysDisabled = isKeyDisablerActive(this);
             mNavKeys.setChecked(navKeysDisabled);
+        } else {
+            navKeysRow.setVisibility(View.GONE);
         }
 
         View privacyGuardRow = findViewById(R.id.privacy_guard);
@@ -164,7 +158,7 @@ public class MoKeeSettingsActivity extends BaseSetupWizardActivity {
     }
 
     private void updateDisableNavkeysOption() {
-        if (!mHideNavKeysRow) {
+        if (mSupportsKeyDisabler) {
             final Bundle myPageBundle = mSetupWizardApp.getSettingsBundle();
             boolean enabled = MKSettings.Global.getInt(getContentResolver(),
                     MKSettings.Global.DEV_FORCE_SHOW_NAVBAR, 0) != 0;
@@ -187,9 +181,9 @@ public class MoKeeSettingsActivity extends BaseSetupWizardActivity {
         bundle.putBoolean(KEY_PRIVACY_GUARD, checked);
     }
 
-    private static boolean hideKeyDisabler(Context context) {
+    private static boolean isKeyDisablerSupported(Context context) {
         final MKHardwareManager hardware = MKHardwareManager.getInstance(context);
-        return !hardware.isSupported(MKHardwareManager.FEATURE_KEY_DISABLE);
+        return hardware.isSupported(MKHardwareManager.FEATURE_KEY_DISABLE);
     }
 
     private static boolean isKeyDisablerActive(Context context) {
