CalyxOS/platform_packages_apps_SetupWizard

Hide activities when setup finishes, so they don't pop up at the end

src/org/calyxos/setupwizard/BaseSetupWizardActivity.java
@@ -17,12 +17,14 @@
 
 package org.calyxos.setupwizard;
 
+import static android.view.View.INVISIBLE;
 import static com.android.setupwizardlib.util.ResultCodes.RESULT_ACTIVITY_NOT_FOUND;
 import static com.android.setupwizardlib.util.ResultCodes.RESULT_RETRY;
 import static com.android.setupwizardlib.util.ResultCodes.RESULT_SKIP;
 
 import static org.calyxos.setupwizard.SetupWizardApp.ACTION_EMERGENCY_DIAL;
 import static org.calyxos.setupwizard.SetupWizardApp.ACTION_NEXT;
+import static org.calyxos.setupwizard.SetupWizardApp.ACTION_SETUP_COMPLETE;
 import static org.calyxos.setupwizard.SetupWizardApp.EXTRA_ACTION_ID;
 import static org.calyxos.setupwizard.SetupWizardApp.EXTRA_FIRST_RUN;
 import static org.calyxos.setupwizard.SetupWizardApp.EXTRA_HAS_MULTIPLE_USERS;
@@ -36,7 +38,10 @@ import android.annotation.Nullable;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.ActivityNotFoundException;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 import android.net.wifi.WifiManager;
@@ -85,6 +90,18 @@ public abstract class BaseSetupWizardActivity extends Activity implements Naviga
     private boolean mIsPrimaryUser;
     private int mResultCode = 0;
     private Intent mResultData;
+    private final BroadcastReceiver finishReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (ACTION_SETUP_COMPLETE.equals(intent.getAction())) {
+                if (BaseSetupWizardActivity.this instanceof FinishActivity) return;
+                if (mNavigationBar != null) {
+                    // hide the activity's view, so it does not pop up again
+                    mNavigationBar.getView().getRootView().setVisibility(INVISIBLE);
+                }
+            }
+        }
+    };
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -92,6 +109,7 @@ public abstract class BaseSetupWizardActivity extends Activity implements Naviga
             logActivityState("onCreate savedInstanceState=" + savedInstanceState);
         }
         super.onCreate(savedInstanceState);
+        registerReceiver(finishReceiver, new IntentFilter(ACTION_SETUP_COMPLETE));
         mIsPrimaryUser = UserHandle.myUserId() == 0;
         initLayout();
     }
@@ -149,6 +167,7 @@ public abstract class BaseSetupWizardActivity extends Activity implements Naviga
         if (LOGV) {
             logActivityState("onDestroy");
         }
+        unregisterReceiver(finishReceiver);
         super.onDestroy();
     }
 
@@ -260,7 +279,7 @@ public abstract class BaseSetupWizardActivity extends Activity implements Naviga
             Animation fadeOut = AnimationUtils.loadAnimation(this, android.R.anim.fade_out);
             final Button next = mNavigationBar.getNextButton();
             next.startAnimation(fadeOut);
-            next.setVisibility(View.INVISIBLE);
+            next.setVisibility(INVISIBLE);
         }
     }
 
@@ -468,7 +487,7 @@ public abstract class BaseSetupWizardActivity extends Activity implements Naviga
             Animation fadeOut = AnimationUtils.loadAnimation(this, android.R.anim.fade_out);
             final Button back = mNavigationBar.getBackButton();
             back.startAnimation(fadeOut);
-            back.setVisibility(View.INVISIBLE);
+            back.setVisibility(INVISIBLE);
         }
     }
 

src/org/calyxos/setupwizard/FinishActivity.java
@@ -17,25 +17,18 @@
 
 package org.calyxos.setupwizard;
 
-import static org.calyxos.setupwizard.SetupWizardApp.DISABLE_NAV_KEYS;
-import static org.calyxos.setupwizard.SetupWizardApp.KEY_BUTTON_BACKLIGHT;
-import static org.calyxos.setupwizard.SetupWizardApp.KEY_PRIVACY_GUARD;
-import static org.calyxos.setupwizard.SetupWizardApp.KEY_SEND_METRICS;
+import static org.calyxos.setupwizard.SetupWizardApp.ACTION_SETUP_COMPLETE;
 import static org.calyxos.setupwizard.SetupWizardApp.LOGV;
 
 import android.animation.Animator;
 import android.app.Activity;
 import android.app.WallpaperManager;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.UserHandle;
-import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.widget.ImageView;
@@ -43,6 +36,9 @@ import android.widget.ImageView;
 import com.android.setupwizardlib.util.WizardManagerHelper;
 
 import org.calyxos.setupwizard.util.EnableAccessibilityController;
+
+import static android.os.Binder.getCallingUserHandle;
+import static org.calyxos.setupwizard.Manifest.permission.FINISH_SETUP;
 
 public class FinishActivity extends BaseSetupWizardActivity {
 
@@ -102,6 +98,10 @@ public class FinishActivity extends BaseSetupWizardActivity {
     }
 
     private void startFinishSequence() {
+        Intent i = new Intent(ACTION_SETUP_COMPLETE);
+        i.setPackage(getPackageName());
+        sendBroadcastAsUser(i, getCallingUserHandle(), FINISH_SETUP);
+
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LOCKED);
         hideBackButton();
         hideNextButton();

src/org/calyxos/setupwizard/SetupWizardApp.java
@@ -35,6 +35,7 @@ public class SetupWizardApp extends Application {
     /* Verbose Logging */
     public static final boolean LOGV = Log.isLoggable(TAG, Log.VERBOSE);
 
+    public static final String ACTION_SETUP_COMPLETE = "org.calyxos.setupwizard.CALYX_SETUP_COMPLETE";
     public static final String ACTION_FINISHED = "org.calyxos.setupwizard.SETUP_FINISHED";
     public static final String ACTION_SETUP_WIFI = "android.net.wifi.PICK_WIFI_NETWORK";
     public static final String ACTION_SETUP_FINGERPRINT = "android.settings.FINGERPRINT_SETUP";

MoKee/android_packages_apps_SetupWizard

Change-Id: Ica6b5babf7df7a7d8033ecd7694e3f45144b363a

src/org/mokee/setupwizard/BaseSetupWizardActivity.java
@@ -17,12 +17,14 @@
 
 package org.mokee.setupwizard;
 
+import static android.view.View.INVISIBLE;
 import static com.google.android.setupcompat.util.ResultCodes.RESULT_ACTIVITY_NOT_FOUND;
 import static com.google.android.setupcompat.util.ResultCodes.RESULT_RETRY;
 import static com.google.android.setupcompat.util.ResultCodes.RESULT_SKIP;
 
 import static org.mokee.setupwizard.SetupWizardApp.ACTION_EMERGENCY_DIAL;
 import static org.mokee.setupwizard.SetupWizardApp.ACTION_NEXT;
+import static org.mokee.setupwizard.SetupWizardApp.ACTION_SETUP_COMPLETE;
 import static org.mokee.setupwizard.SetupWizardApp.EXTRA_ACTION_ID;
 import static org.mokee.setupwizard.SetupWizardApp.EXTRA_FIRST_RUN;
 import static org.mokee.setupwizard.SetupWizardApp.EXTRA_HAS_MULTIPLE_USERS;
@@ -36,7 +38,10 @@ import android.annotation.Nullable;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.ActivityNotFoundException;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 import android.net.wifi.WifiManager;
@@ -91,6 +96,18 @@ public abstract class BaseSetupWizardActivity extends Activity implements Naviga
     private boolean mIsPrimaryUser;
     protected int mResultCode = 0;
     private Intent mResultData;
+    private final BroadcastReceiver finishReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (ACTION_SETUP_COMPLETE.equals(intent.getAction())) {
+                if (BaseSetupWizardActivity.this instanceof FinishActivity) return;
+                if (mNavigationBar != null) {
+                    // hide the activity's view, so it does not pop up again
+                    mNavigationBar.getRootView().setVisibility(INVISIBLE);
+                }
+            }
+        }
+    };
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -98,6 +115,7 @@ public abstract class BaseSetupWizardActivity extends Activity implements Naviga
             logActivityState("onCreate savedInstanceState=" + savedInstanceState);
         }
         super.onCreate(savedInstanceState);
+        registerReceiver(finishReceiver, new IntentFilter(ACTION_SETUP_COMPLETE));
         mIsPrimaryUser = UserHandle.myUserId() == 0;
         initLayout();
         mNavigationBar = getNavigationBar();
@@ -169,6 +187,7 @@ public abstract class BaseSetupWizardActivity extends Activity implements Naviga
         if (LOGV) {
             logActivityState("onDestroy");
         }
+        unregisterReceiver(finishReceiver);
         super.onDestroy();
     }
 
@@ -307,7 +326,7 @@ public abstract class BaseSetupWizardActivity extends Activity implements Naviga
             Animation fadeOut = AnimationUtils.loadAnimation(this, android.R.anim.fade_out);
             final Button next = mNavigationBar.getNextButton();
             next.startAnimation(fadeOut);
-            next.setVisibility(View.INVISIBLE);
+            next.setVisibility(INVISIBLE);
         }
     }
 
@@ -515,7 +534,7 @@ public abstract class BaseSetupWizardActivity extends Activity implements Naviga
             Animation fadeOut = AnimationUtils.loadAnimation(this, android.R.anim.fade_out);
             final Button back = mNavigationBar.getBackButton();
             back.startAnimation(fadeOut);
-            back.setVisibility(View.INVISIBLE);
+            back.setVisibility(INVISIBLE);
         }
     }
 

src/org/mokee/setupwizard/FinishActivity.java
@@ -18,6 +18,7 @@
 
 package org.mokee.setupwizard;
 
+import static org.mokee.setupwizard.SetupWizardApp.ACTION_SETUP_COMPLETE;
 import static org.mokee.setupwizard.SetupWizardApp.DISABLE_NAV_KEYS;
 import static org.mokee.setupwizard.SetupWizardApp.LOGV;
 
@@ -43,6 +44,9 @@ import com.google.android.setupcompat.util.WizardManagerHelper;
 import org.mokee.setupwizard.util.EnableAccessibilityController;
 
 import mokee.providers.MoKeeSettings;
+
+import static android.os.Binder.getCallingUserHandle;
+import static org.mokee.setupwizard.Manifest.permission.FINISH_SETUP;
 
 public class FinishActivity extends BaseSetupWizardActivity {
 
@@ -99,6 +103,10 @@ public class FinishActivity extends BaseSetupWizardActivity {
     }
 
     private void startFinishSequence() {
+        Intent i = new Intent(ACTION_SETUP_COMPLETE);
+        i.setPackage(getPackageName());
+        sendBroadcastAsUser(i, getCallingUserHandle(), FINISH_SETUP);
+
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LOCKED);
         hideBackButton();
         hideNextButton();

src/org/mokee/setupwizard/SetupWizardApp.java
@@ -35,6 +35,7 @@ public class SetupWizardApp extends Application {
     /* Verbose Logging */
     public static final boolean LOGV = Log.isLoggable(TAG, Log.VERBOSE);
 
+    public static final String ACTION_SETUP_COMPLETE = "org.mokee.setupwizard.MOKEE_SETUP_COMPLETE";
     public static final String ACTION_FINISHED = "org.mokee.setupwizard.SETUP_FINISHED";
     public static final String ACTION_SETUP_WIFI = "android.net.wifi.PICK_WIFI_NETWORK";
     public static final String ACTION_SETUP_FINGERPRINT = "android.settings.FINGERPRINT_SETUP";